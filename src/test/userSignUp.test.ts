ded5e12dc870005e7b8a971d0653232ff8741625
Javi <javiermespino@gmail.com>
2023-03-28 13:52:51 +0000

    Modified database class

--- src/test/userSignUp.test.ts
+++ src/test/userSignUp.test.ts
@@ -1,10 +1,10 @@
-import { FakeDatabase } from '../core/fakeDataBase';
+import { FakeDatabase } from '../core/fakeDatabase';
 import { UserService } from '../core/userService';
 
 describe('User Sign Up Test', () => {
     it('should not add user if email is empty', () => {
-        const fakeDataBase = new FakeDatabase();
-        const service = new UserService(fakeDataBase);
+        const fakeDatabase = new FakeDatabase();
+        const service = new UserService(fakeDatabase);
         const spy = jest.spyOn(service, 'save');
         service.save('');
 
