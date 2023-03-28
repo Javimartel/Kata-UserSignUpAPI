fc8da359e220e9b3d8df09e52550fa6d044c107e
Javi <javiermespino@gmail.com>
2023-03-28 14:14:49 +0000

    Test(green): should_throw_error_if_email_has_incorrect_format

--- src/core/userService.ts
+++ src/core/userService.ts
@@ -6,6 +6,10 @@ export class UserService {
     save(email: string) {
         if (email !== '') {
             this.userRepository.save(email);
+        } 
+        
+        if (email && !email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            throw new Error(`"${email}" has incorrect format`);
         }
     }
 
