cff8f44bbbab4d0e9aeb023ac36e4e9200cbae0d
Javi <javiermespino@gmail.com>
2023-03-28 14:05:56 +0000

    Test(red): should_throw_error_if_email_has_incorrect_format

--- src/test/userSignUp.test.ts
+++ src/test/userSignUp.test.ts
@@ -6,11 +6,24 @@ describe('User Sign Up Test', () => {
         const fakeDatabase = new FakeDatabase();
         const service = new UserService(fakeDatabase);
         const spy = jest.spyOn(service, 'save');
-        service.save('');
+        const email = '';
+        service.save(email);
 
         const users = service.getUsers();
 
         expect(spy).toHaveBeenCalled();
         expect(users.length).toBe(0);
+    });
+    
+    it('should throw error if email has incorrect format', () => {
+        const fakeDatabase = new FakeDatabase();
+        const service = new UserService(fakeDatabase);
+        const spy = jest.spyOn(service, 'save');
+        const email = 'email.com';
+        
+        expect(() => {
+            service.save(email);
+        }).toThrow(`"${email}" has incorrect format`);
+        expect(spy).toHaveBeenCalled();
     });
 });
